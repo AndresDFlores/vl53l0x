c603d72e17a4fa94c674af4eaa1777eac958098c
Andres Flores <dflores.andres@gmail.com>
2026-08-20 21:54:13 -0400

    designed laser gate class implemeentation

--- laser_gate.py
+++ laser_gate.py
@@ -1,31 +1,65 @@
 import time
+from collections import deque
 from vl53l0x_module import *
 
 
-module_class = vl53l0xModule(scl_pin=1, sda_pin=0, freq=400000)
-module_class.start_module()
+class LaserGate:
 
-iter_delay = 0.05
+    #  use VL53L0X to log when objects pass through the chute slide
 
-try:
+    def __init__(self):
 
-    setpoint_data = []
-    for _ in range(10):
-        distance_mm = module_class.time_of_flight.read()
-        setpoint_data.append(distance_mm)
+        #  start vl53l0x module
+        self.module_class = vl53l0xModule(scl_pin=1, sda_pin=0, freq=400000)
+        self.module_class.start_module()
 
-    setpoint = sum(setpoint_data)/len(setpoint_data)
+        #  class variables
+        self.iter_delay = 0.05  # how long between each data read
+        self.gate_flag = deque([False, False],2)  # track gate crossing
 
 
-    while True:
-        distance_mm = module_class.time_of_flight.read()
+    def monitor_gate(self):
 
-        if distance_mm<(setpoint*0.95):
-            #  telemetry log note
-            print(f'LASER TRIPPED: {distance_mm}mm')
+        try:
+
+            #  establish a gate beam length setpoint as an average measurement, tof
+            setpoint_data = []
+            for _ in range(10):
+                distance_mm = self.module_class.time_of_flight.read()
+                setpoint_data.append(distance_mm)
+
+            setpoint = sum(setpoint_data)/len(setpoint_data)
+            #  log setpoint value and time measured
+
+
+            #  continually monitor gate for bean interruptions
+            while True:
+                distance_mm = self.module_class.time_of_flight.read()
+
+
+                #  beam interruption if measured distance is less than 95% of setpoint
+                if distance_mm<(setpoint*0.95):
+                    #  telemetry log note
+                    self.gate_flag.append(True)
+                else:
+                    self.gate_flag.append(False)
+
+
+                #  compare current iter flag to previous iter flag to determine interruption status
+                flag_sum = self.gate_flag[-1]-self.gate_flag[0]
+                if flag_sum==1:
+                    print(f'Object Crossed Laser Gate')
+                    #  log gate cross event time 
+
+
+                #  wait until executing next loop
+                time.sleep(self.iter_delay)
+
             
-        time.sleep(iter_delay)
-    
-except KeyboardInterrupt:
-    module_class.time_of_flight.stop()
-    print("Stopped.")
+        except KeyboardInterrupt:
+            self.module_class.time_of_flight.stop()
+            print("Stopped.")
+
+
+if __name__=="__main__":
+    LaserGate().monitor_gate()
